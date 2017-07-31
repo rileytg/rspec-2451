073997d67505e0b19c7248816d2a8e07d7143394
Riley Guerin <rileytg@users.noreply.github.com>
2017-07-30 14:15:26 -1000

    basic test files

---
+++ spec/bar_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe "bar" do
+  it "foo" do
+    expect(true).to be_truthy
+  end
+end
+
---
+++ spec/foo_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe "foo" do
+  it "bar" do
+    expect(true).to be_truthy
+  end
+end
+
