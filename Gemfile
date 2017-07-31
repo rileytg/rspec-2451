a1f80fc8ce2b4d81df10fc967081ac77dcf6dfb9
Riley Guerin <rileytg@users.noreply.github.com>
2017-07-30 14:49:49 -1000

    add junit formatter

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
- gem "rspec"
+gem "rspec"
+gem "rspec-core", "3.5.4"
+gem "rspec_junit_formatter"
--- cause-error.sh
+++ cause-error.sh
@@ -1,1 +1,5 @@
-bundle exec rspec "spec/foo_spec.rb spec/bar_spec.rb"
+bundle exec rspec --format progress \
+                --format RspecJunitFormatter \
+                --out /tmp/test-results/rspec.xml \
+                --format progress \ 
+		"spec/foo_spec.rb spec/bar_spec.rb"
